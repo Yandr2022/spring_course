a65a5c178f2b2b9e817e3e690af70563235118ca
Yandr2022 <sharkievich@gmail.com>
2023-03-14 03:50:49 +0300

    hibernate: added one-to-one relation

--- src/main/java/hibernate_one_to_one/Test2.java
+++ src/main/java/hibernate_one_to_one/Test2.java
@@ -1,13 +1,13 @@
-package hibernate_test2;
+package hibernate_one_to_one;
 
-import hibernate_test2.entity.Detail;
-import hibernate_test2.entity.Employee;
+import hibernate_one_to_one.entity.Detail;
+import hibernate_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class).addAnnotatedClass(Detail.class).buildSessionFactory();
@@ -15,8 +15,9 @@ public class Test1 {
         try {
            session = factory.getCurrentSession();
             session.beginTransaction();
-            Employee employee = session.get(Employee.class,2);
-            session.delete(employee);
+            Detail detail = session.get(Detail.class,2);
+            detail.getEmployee().setEmpDetail(null);
+            session.delete(detail);
             session.getTransaction().commit();
         } finally {
             session.close();
